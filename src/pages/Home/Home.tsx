f34382154579785781d22f3b8c640ad5e4e1c075
2625-glitch <vennapureddypooji@gmail.com>
2023-05-23 09:30:13 +0530

    home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ErrorBoundary, Navbar } from '../../components';
+import Homenav from './Homenav';
+import Homeindex from './Homeindex';
 
 const Home = () => {
+    const [selectedPage, setSelectedPage] = useState<string>('Home');
+    const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            //we are at top of page
+            if (window.scrollY === 0) {
+                setIsTopOfPage(true);
+                setSelectedPage('Home');
+            }
+            if (window.scrollY !== 0) {
+                setIsTopOfPage(false);
+            }
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
     return (
         <ErrorBoundary>
-            <h1 style={{ marginLeft: 100 }}>
-                Welcome to valExpert, We can start now
-            </h1>
+            <Homenav
+                isTopOfPage={isTopOfPage}
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+            />
+            <Homeindex setSelectedPage={setSelectedPage} />
         </ErrorBoundary>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,21 @@ module.exports = {
   theme: {
     extend: {},
   },
+  colors:{
+    "gray-20":"#F8F4EB",
+    "gray-50":"#EFE6E6",
+    "gray-100":"#DFCCCC",
+    "gray-500":"#5E0000",
+    "primary-100":"#FFE1E0",
+    "primary-300":"#FFA6A3",
+    "primary-500":"#FF6B66",
+    "secondary-400":"#FFCD58",
+    "secondary-500":"#FFC132",
+  },
+  fontFamily:{
+    dmsans:["DM Sans","sans-serif"],
+    montserrat:["Montserrat","sans-serif"]
+  },
   variants: {
     extend: {},
   },
