51c3d40a732aaa962e430e5d860106b4ca75e346
amrathmd <amrathmd.mohammed@gmail.com>
2023-06-08 22:25:05 +0530

    error

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,6 +2,6 @@ export { default as Home } from './Home/Home';
 export { default as Login } from './Login/Login';
 export { default as Contact } from './Contact/Contact';
 export { default as Homepage } from './Homepage/Homepage';
-export { default as Register } from './Register-step1/Register';
 export { default as Register2 } from './Register-step2/Register';
+export { default as Register1 } from './Register-step1/Register';
 export { default as RegistrationPage } from './RegistrationPage/RegistrationPage';
