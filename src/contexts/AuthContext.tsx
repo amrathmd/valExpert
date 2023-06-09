44ac36605c774102dd572a6020af79ed773dd41c
amrathmd <amrathmd.mohammed@gmail.com>
2023-06-09 22:10:49 +0530

    test

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -6,20 +6,32 @@ const AuthContext = createContext(null);
 
 export const AuthContextProvider = (props: any) => {
     const [loggedIn, setLoggedIn] = useState<boolean>(false);
+    const [userType, setUserType] = useState(null);
 
     const getLoggedIn = async () => {
         const result = await axios.get(
             'http://localhost:3000/v1/auth/loggedIn'
         );
         setLoggedIn(result.data);
+        console.log(result);
+    };
+    const findUserType = async () => {
+        const result = await axios.get(
+            'http://localhost:3000/v1/auth/userType'
+        );
+        setUserType(result.data);
     };
 
     React.useEffect(() => {
         getLoggedIn();
     }, []);
+    React.useEffect(() => {
+        findUserType();
+        console.log(userType);
+    }, [loggedIn]);
 
     return (
-        <AuthContext.Provider value={{ loggedIn, getLoggedIn }}>
+        <AuthContext.Provider value={{ loggedIn, getLoggedIn, userType }}>
             {props.children}
         </AuthContext.Provider>
     );
