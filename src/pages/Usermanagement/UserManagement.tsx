635be3f70831959d22ff3694ed5a14b3219afb35
amrathmd <amrathmd.mohammed@gmail.com>
2023-07-27 19:32:05 +0530

    usermanagement

--- src/pages/Usermanagement/UserManagement.tsx
+++ src/pages/Usermanagement/UserManagement.tsx
@@ -1,6 +1,62 @@
 import React from 'react';
-
+import axios from 'axios';
+import UserForm from './UserForm';
+import './user.css';
+interface User {
+    _id: string;
+    name: string;
+    mobile: string;
+    email: string;
+    status: string;
+}
 const UserManagement = () => {
-    return <div>You are on usermanagement</div>;
+    const [users, setUsers] = React.useState([]);
+    const [userprompt, setUserprompt] = React.useState<boolean>();
+    const handleUserPrompt = () => {
+        setUserprompt(!userprompt);
+    };
+    /* const getUsers = async () => {
+        const res = await axios.get('http://localhost:3000/v1/adminusers');
+        console.log(res);
+        setUsers(res.data);
+        console.log(users);
+    };
+    React.useEffect(() => {
+        getUsers();
+    }, []);*/
+    return (
+        <div>
+            {users.length === 0 && (
+                <div className="message">
+                    <div>
+                        <p className="para">Welcome to our platform!</p>
+                        <ul className="dot-list">
+                            <li>
+                                You have the power to add users from your
+                                company enabling
+                                <br /> effective collaboration Enhanced Project
+                                Management,Customized User Permissions.
+                            </li>
+                            <li>
+                                We believe that by adding users from your
+                                company,
+                                <br />
+                                you'll unlock the full potential of our platform
+                                .
+                            </li>
+                        </ul>
+                        <i className="fa fa-start-o" aria-hidden="true"></i>
+                    </div>
+                    <span className="create-button" onClick={handleUserPrompt}>
+                        Add Users
+                    </span>
+                    <UserForm
+                        userprompt={userprompt}
+                        handleUserPrompt={handleUserPrompt}
+                    />
+                </div>
+            )}
+        </div>
+    );
 };
 export default UserManagement;
---
+++ src/pages/Usermanagement/UserTable.tsx
@@ -0,0 +1,199 @@
+import React, { useState } from 'react';
+import './user.css';
+import DashboardContext from '../../contexts/dashboardContext';
+interface Users {
+    _id: string;
+    name: string;
+    mobile: string;
+    email: string;
+    status: string;
+}
+
+interface TableProps {
+    users: Users[];
+    onUpdateUsers: (updatedUsers: Users[]) => void;
+    onDeleteUser: (userId: string) => void;
+}
+
+const UserTable: React.FC<TableProps> = (props) => {
+    const { users, onUpdateUsers, onDeleteUser } = props;
+    const { dashboardState, setDashboardState } =
+        React.useContext(DashboardContext);
+    const [isBlinking, setIsBlinking] = useState(false);
+    const [editingUserId, setEditingUserId] = React.useState<string>('');
+    const [editedUserData, setEditedUserData] = React.useState<Partial<Users>>(
+        {}
+    );
+
+    const handleUser = () => {
+        setDashboardState(1);
+    };
+    const handleEditUser = (userId: string) => {
+        setEditingUserId(userId);
+    };
+    const handleDeleteUser = (userId: string) => {
+        const updatedUsers = users.filter((user) => user._id !== userId);
+        onUpdateUsers(updatedUsers);
+    };
+    const handleSaveUser = () => {
+        const editedUserIndex = users.findIndex(
+            (user) => user._id === editingUserId
+        );
+        if (editedUserIndex !== -1) {
+            const updatedUsers = [...users];
+            updatedUsers[editedUserIndex] = {
+                ...updatedUsers[editedUserIndex],
+                ...editedUserData,
+            };
+            // api call- save functionality here
+            onUpdateUsers(updatedUsers);
+            setEditingUserId('');
+            setEditedUserData({});
+        }
+    };
+
+    const handleCancelEdit = () => {
+        setEditingUserId('');
+        setEditedUserData({});
+    };
+
+    const isEditing = (userId: string) => {
+        return editingUserId === userId;
+    };
+    const handleInputChange = (field: keyof Users, value: string) => {
+        setEditedUserData((prevData) => ({
+            ...prevData,
+            [field]: value,
+        }));
+    };
+    return (
+        <table className="content-table">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Mobile</th>
+                    <th>Email</th>
+                    <th>Status</th>
+                    <th>Manage user</th>
+                </tr>
+            </thead>
+            <tbody>
+                {users.map((user) => (
+                    <tr key={user._id}>
+                        <td>{user._id}</td>
+                        <td>
+                            {isEditing(user._id) ? (
+                                <input
+                                    type="text"
+                                    value={editedUserData.name ?? user.name}
+                                    onChange={(e) =>
+                                        handleInputChange(
+                                            'name',
+                                            e.target.value
+                                        )
+                                    }
+                                />
+                            ) : (
+                                user.name
+                            )}
+                        </td>
+                        <td>
+                            {isEditing(user._id) ? (
+                                <input
+                                    type="text"
+                                    value={editedUserData.mobile ?? user.mobile}
+                                    onChange={(e) =>
+                                        handleInputChange(
+                                            'mobile',
+                                            e.target.value
+                                        )
+                                    }
+                                />
+                            ) : (
+                                user.mobile
+                            )}
+                        </td>
+                        <td>
+                            {isEditing(user._id) ? (
+                                <input
+                                    type="text"
+                                    value={editedUserData.email ?? user.email}
+                                    onChange={(e) =>
+                                        handleInputChange(
+                                            'email',
+                                            e.target.value
+                                        )
+                                    }
+                                />
+                            ) : (
+                                user.email
+                            )}
+                        </td>
+                        <td>
+                            {isEditing(user._id) ? (
+                                <input
+                                    type="text"
+                                    value={editedUserData.status ?? user.status}
+                                    onChange={(e) =>
+                                        handleInputChange(
+                                            'status',
+                                            e.target.value
+                                        )
+                                    }
+                                />
+                            ) : (
+                                user.status
+                            )}
+                        </td>
+                        <td>
+                            {isEditing(user._id) ? (
+                                <>
+                                    <div className="action-icon">
+                                        <button
+                                            className="button"
+                                            onClick={handleSaveUser}
+                                        >
+                                            Save
+                                        </button>
+                                        <button
+                                            className="button"
+                                            onClick={handleCancelEdit}
+                                        >
+                                            Cancel
+                                        </button>
+                                    </div>
+                                </>
+                            ) : (
+                                <>
+                                    <div className="action-icon">
+                                        <img
+                                            src="../../../public/edit.png"
+                                            alt="Edit"
+                                            className="edit-icon"
+                                            title="Edit User"
+                                            onClick={() =>
+                                                handleEditUser(user._id)
+                                            }
+                                        />
+                                        <img
+                                            src="../../../public/bin.png"
+                                            alt="Delete"
+                                            className="edit-icon"
+                                            title="Delete  User"
+                                            onClick={() =>
+                                                handleDeleteUser(user._id)
+                                            }
+                                        />
+                                    </div>
+                                </>
+                            )}
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
+
+export default UserTable;
